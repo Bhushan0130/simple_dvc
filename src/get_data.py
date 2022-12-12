b83f2bd9656af0dfd18b5e64f746dd253cab9e05
Bhushan Sharma <bhushansharma@Bhushans-MacBook-Air.local>
2022-12-12 09:44:24 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -12,7 +12,7 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
-def get_data(config_path):
+def get__data(config_path):
     config = read_params(config_path)
     print(config)
     #---------------Fatching Data--------------
@@ -26,5 +26,5 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument('--config', default='params.yaml')
     parsed_args =  args.parse_args()
-    get_data(config_path=parsed_args.config)
+    get__data(config_path=parsed_args.config)
 
